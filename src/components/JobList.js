649a9d7699e847c545dd7963aef502e3c6b24ffb
Michael Kha <mxk5025@rit.edu>
2019-09-21 00:49:49 -0400

    Fixed warnings

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -7,6 +7,7 @@ export default function JobList({jobs}) {
     <ul>
       { jobs.map(job =>
         <JobApp
+          key={job.company + job.position}
           company={job.company}
           position={job.position}
           description={job.description}
