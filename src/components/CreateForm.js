f91bc6d868d4a58853c754f9720f8db16c57acfa
Andrew Lee <axl8175@rit.edu>
2019-09-21 10:04:18 -0400

    Added handlers to CreateForm

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreateForm.css';
 
+
 export default function CreateForm() {
+  const [company, setCompany] = useState('');
+  const [position, setPosition] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleCompany = (e) => {
+    setCompany(e.target.value);
+  };
+
+  const handlePosition = (e) => {
+    setPosition(e.target.value);
+  };
+
+  const handleDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="CreateForm">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="InputContainer">
           Company
-          <input type="text" name="Company" />
+          <input
+            type="text"
+            name="Company"
+            value={company}
+            onChange={handleCompany}
+          />
         </div>
         <div className="InputContainer">
           Position
-          <input type="text" name="Position" />
+          <input
+            type="text"
+            name="Position"
+            value={position}
+            onChange={handlePosition}
+          />
         </div>
         <div className="DescriptionContainer">
           Description
-          <textarea rows="6" cols="50" type="text" name="Description">
-          Enter text here...
-            {' '}
-          </textarea>
+          <textarea
+            rows="6"
+            cols="50"
+            type="text"
+            name="Description"
+            value={description}
+            onChange={handleDescription}
+          />
         </div>
         <div className="SubmitButton">
           <input type="submit" value="Submit" />
