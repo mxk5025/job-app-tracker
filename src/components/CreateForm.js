cf8128d4a3c477cf679c9e6d2ab1d2dee70e54b4
Andrew Lee <axl8175@rit.edu>
2019-09-21 11:14:27 -0400

    Fixed the formating of the description box to fit the screen width

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -55,7 +55,6 @@ export default function CreateForm({ status, addJobApp }) {
           Description
           <textarea
             rows="6"
-            cols="50"
             type="text"
             name="Description"
             value={description}
