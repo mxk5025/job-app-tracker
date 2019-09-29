6ea819ace9f7c7f0c5fad25e9f2d274c7c9e3cd7
Michael Kha <mxk5025@rit.edu>
2019-09-29 15:57:04 -0400

    Updates

---
+++ precache-manifest.9e97a33c931aad52b7eeae7a49172455.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e95c5c6b0f00035829e128e9b9cacb",
+    "url": "/job-app-tracker/index.html"
+  },
+  {
+    "revision": "95788742690b9eef6367",
+    "url": "/job-app-tracker/static/css/main.3f0f84de.chunk.css"
+  },
+  {
+    "revision": "a61df86fdbd14f645c78",
+    "url": "/job-app-tracker/static/js/2.6f51dee8.chunk.js"
+  },
+  {
+    "revision": "95788742690b9eef6367",
+    "url": "/job-app-tracker/static/js/main.301bc016.chunk.js"
+  },
+  {
+    "revision": "619dbcfa4e1739434d5e",
+    "url": "/job-app-tracker/static/js/runtime~main.4aa846ab.js"
+  }
+]);
